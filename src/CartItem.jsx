dc9da940ff4a39d382b1bb2a9491259987d2541c
stevenandz-glitch <stevenandz92491@gmail.com>
2026-02-22 11:10:46 -0500

    made some more changes

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,39 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total_count = 0;
+    cart.forEach((item) => total_count += item.quantity * parseInt(item.cost.substring(1)));
+    return total_count;
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
-
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 0) {
+      dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+    } else {
+      dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
+
+  const handleCheckoutShopping = (e) => {
+    alert("Super awesome functionality coming your way");
+  }
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity * parseInt(item.cost.substring(1));
   };
 
   return (
